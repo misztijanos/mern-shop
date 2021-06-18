70409a9a8fc8741e37588299603df146aa2308c4
misztijanos <misztijanos@yahoo.com>
2021-06-18 17:18:27 +0300

    server, db, mongoose models added

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom"
+import { Card } from "react-bootstrap"
+import Rating from "./Rating"
+
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top"></Card.Img>
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+          <Card.Text as="div">
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
+          </Card.Text>
+          <Card.Text as="h3" className="product-price">
+            ${product.price}
+          </Card.Text>
+        </Link>
+      </Card.Body>
+    </Card>
+  )
+}
+export default Product
